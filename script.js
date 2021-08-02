b8d8c51c9d523acd848226d293293f4b5dfaf31f
Fawzaan <fawzi20112001@gmail.com>
2021-08-02 21:14:47 +0530

    Burger menu elements added with scripts

--- script.js
+++ script.js
@@ -5,18 +5,31 @@
  */
 
 
-/* Global Variables */
+/* --- Global Variables --- */
 const doc = document;
 var burgerMenuClicked = false;
+var burgerMenuLinkClicked = false;
 
-/* Header Functions */
-// For mobile mode
-function selectBurgerMenu() {   
+/* --- Header Functions --- */
+/* For mobile mode */
+function onSelectBurgerMenuClick() {   
     if (burgerMenuClicked == false) {
         doc.getElementById("burger-menu").style.backgroundColor = "rgb(0, 186, 0)";
+        doc.getElementById("burger-menu-list").style.display = "block";
         burgerMenuClicked = true;
     } else {
         doc.getElementById("burger-menu").style.backgroundColor = "rgb(0, 206, 0)";
+        doc.getElementById("burger-menu-list").style.display = "none";
         burgerMenuClicked = false;
+    }
+}
+
+function onBurgerMenuLinkClick(link) {
+    if (burgerMenuLinkClicked == false) {
+        link.style.backgroundColor = "rgb(255, 205, 0)";
+        burgerMenuLinkClicked = true;
+    } else {
+        link.style.backgroundColor = "rgb(244, 244, 244)";
+        burgerMenuLinkClicked = false;
     }
 }
