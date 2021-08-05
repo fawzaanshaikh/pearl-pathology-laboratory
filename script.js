bd4ae835be9f21e2d07c2c95c49569f0e6454ac5
Fawzaan <fawzi20112001@gmail.com>
2021-08-05 13:35:43 +0530

    Added picture slideshow in computer mode

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
  *  ---------- Directory ----------
  * 1. Global Variables
  * 2. Header Functions
+ * 3. Slideshow Functions 
  */
 
 
@@ -9,6 +10,7 @@
 const doc = document;
 var burgerMenuClicked = false;
 var burgerMenuLinkClicked = false;
+
 
 /* --- Header Functions --- */
 /* For mobile mode */
@@ -48,4 +50,35 @@ function onNavbarLinkClick(link, linkType) {
     } else {
         burgerMenuLinkClicked = false;
     }
+}
+
+
+/* --- Slideshow Functions --- */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideshow-slides");
+  var dots = document.getElementsByClassName("slideshow-dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
